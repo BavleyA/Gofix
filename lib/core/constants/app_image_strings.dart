476f3770688cc635f061d698f18c48f8d3a7cff6
BavleyA <paula3del@gmail.com>
2025-09-29 22:34:16 +0300

    - add image strings

--- lib/core/constants/app_image_strings.dart
+++ lib/core/constants/app_image_strings.dart
@@ -1,4 +1,19 @@
 class AppImageStrings {
 
-
+  static const appLogo = 'assets/images/AppLogoCircular.png';
+  static const chooseRoleDriver = 'assets/images/chooseRoleDriverImage.png';
+  static const chooseRoleTrader = 'assets/images/chooseRoleSellerImage.png';
+  static const car = 'assets/images/carImage.png';
+  static const motorcycle = 'assets/images/motorcycle.png';
+  static const van = 'assets/images/van.png';
+  static const bicycle = 'assets/images/bicycle.png';
+  static const profileIcon = 'assets/images/profilePicIcon.png';
+  static const documentIcon = 'assets/images/documentFrame.png';
+  static const nationalIdIcon = 'assets/images/nationalIdImage.png';
+  static const criminalRecordCertificateIcon = 'assets/images/criminalRecordCertificateImage.png';
+  static const vehicleLicenseIcon = 'assets/images/veichleLicenseImage.png';
+  static const driverLicenseIcon = 'assets/images/drivingLicenceImage.png';
+  static const successRegistration = 'assets/images/successRegisteredUnderReviewImage.png';
+  static const businessDetailsIcon = 'assets/images/BuisnessDetailsImageInSellerRegister.png';
+  static const onlinePresenceIcon = 'assets/images/OnlinePresenceImageInSellerRegistration.png';
 }
