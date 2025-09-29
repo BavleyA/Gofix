c36bf0e5995cdce00497fd6e05ecaa0af5efd5ff
BavleyA <paula3del@gmail.com>
2025-09-29 18:06:12 +0300

    - all app strings added

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,41 @@
+class AppStrings{
+
+  static const String appName = 'GoFix';
+  static const String loginTitle = 'Log In';
+  static const String alreadyHaveAccountText = 'Already have an account?';
+  static const String dontHaveAccountText = "Don't have an account?";
+  static const String enterPhoneNumber = 'Enter your phone number';
+  static const String phoneNumberHint = 'Phone Number';
+  static const String continueText = 'Continue';
+  static const String fullNameHint = 'Full Name';
+  static const String emailHint = 'Email';
+  static const String passwordHint = 'Password';
+  static const String nextStepText = 'Next Step';
+  static const String enterCode = 'Enter Code';
+  static const String chooseRole = 'Choose Your Role';
+  static const String driver = 'Driver';
+  static const String trader = 'Trader';
+  static const String selectTheVehicleType = 'Please select the vehicle type';
+  static const String vehicleTypeCar = 'Car';
+  static const String vehicleTypeVan = 'Van';
+  static const String vehicleTypeBike = 'Bicycle';
+  static const String vehicleTypeMotorcycle = 'Motorcycle';
+  static const String requiredDocuments = 'Required Documents';
+  static const String profilePhoto = 'Profile Picture';
+  static const String nationalID = 'National ID';
+  static const String criminalRecordCertificate = 'Criminal record certificate';
+  static const String vehicleLicense = 'Vehicle License';
+  static const String driverLicense = 'Driving License';
+  static const String businessDetails = 'Business Details';
+  static const String onlinePresence = 'Online Presence';
+  static const String businessNameHint = 'Business Name';
+  static const String businessAddressHint = 'Business Address';
+  static const String businessCategoryHint = 'Business Category';
+  static const String front = 'Front';
+  static const String back = 'Back';
+  static const String upload = 'Upload';
+  static const String underReview = 'Under Review';
+  static const String reviewSubtitle = 'We’ve received your information , You will be notified once the review is complete.';
+
+
+}
